c41c8747b8760fa2e91be449a1ebe10389826ed3
pjozsef <9980358+pjozsef@users.noreply.github.com>
2020-03-31 22:34:07 +0200

    feat(backend): add basic backend api

--- word-generator-web/backend/src/main/kotlin/com/github/pjozsef/wordgenerator/web/Main.kt
+++ word-generator-web/backend/src/main/kotlin/com/github/pjozsef/wordgenerator/web/Main.kt
@@ -1,2 +1,65 @@
 package com.github.pjozsef.wordgenerator.web
 
+import com.github.pjozsef.randomtree.io.readTreeFromString
+import com.github.pjozsef.wordgenerator.generateWord
+import com.ryanharter.ktor.moshi.moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.application.log
+import io.ktor.features.ContentNegotiation
+import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.routing.post
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import java.util.*
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+@OptIn(ExperimentalTime::class)
+fun main() {
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            post("/api/generate") {
+                val dto = call.receive<GenerateWordDto>()
+
+                val results = generate(dto).also {
+                    log.info("Word generation of ${dto.times} word${if(dto.times>1) "s" else ""} took ${it.duration.toLongMilliseconds()} millis.")
+                }.value
+
+                call.respond(GenerateWordResponseDto(results, dto.seed))
+            }
+        }
+        install(ContentNegotiation) {
+            moshi {
+                add(Date::class.java, Rfc3339DateJsonAdapter())
+            }
+        }
+    }
+    server.start(wait = true)
+}
+
+@OptIn(ExperimentalTime::class)
+private fun generate(dto: GenerateWordDto) = with(dto) {
+    val random = seed.toLong(36).let(::Random)
+    measureTimedValue {
+        (1..times).map {
+            generateWord(
+                expression(dto, random),
+                mappings,
+                random
+            )
+        }
+    }
+}
+
+private fun expression(dto: GenerateWordDto, random: Random) = dto.category?.let {
+    readTreeFromString(
+        dto.command.replace("\t", "  "),
+        { it },
+        { it.toSortedMap().values.joinToString(" ") },
+        random
+    ).getValue(it).value
+} ?: dto.command
--- word-generator-web/backend/src/main/kotlin/com/github/pjozsef/wordgenerator/web/dtos.kt
+++ word-generator-web/backend/src/main/kotlin/com/github/pjozsef/wordgenerator/web/dtos.kt
@@ -1,11 +1,20 @@
 package com.github.pjozsef.wordgenerator.web
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class GenerateWordDto(
     val command: String,
     val mappings: Map<String, List<String>>,
     val times: Int = 1,
     val category: String? = null,
     val seed: String = random()
+)
+
+@JsonClass(generateAdapter = true)
+data class GenerateWordResponseDto(
+    val results: List<String>,
+    val seed: String
 )
 
 private fun random() = System.currentTimeMillis()
