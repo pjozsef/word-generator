35aa19bef9fb96f3ddd01d55b8ae690951f0a8cd
pjozsef <9980358+pjozsef@users.noreply.github.com>
2020-05-02 20:30:20 +0200

    feat(web-fe): add proxying to backend api endpoints

--- word-generator-web/frontend/server/server.js
+++ word-generator-web/frontend/server/server.js
@@ -1,15 +1,24 @@
 const path = require('path');
 const express = require('express');
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
 const app = express();
-const port = process.env.PORT || 8008;
 const publicPath = path.join(__dirname, '..', 'build');
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
+const port = process.env.PORT || 8008;
+const apiUrl = process.env.API_URL || 'http://localhost:8080';
+
+app.all('/api/*', (req, res)=> {
+    apiProxy.web(req, res, {target: apiUrl});
+})
+
+app.get('/*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
- });
+});
 
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}!`);
- });
+    console.log(`Server is up on port ${port}`);
+    console.log(`Proxying to ${apiUrl}`);
+});
