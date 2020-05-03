849ba1ceaf80664847efffbdda135260afaa0b43
pjozsef <9980358+pjozsef@users.noreply.github.com>
2020-05-03 11:42:24 +0200

    feat(web-fe): use miu styling in header

--- word-generator-web/frontend/src/components/header.tsx
+++ word-generator-web/frontend/src/components/header.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './header.css'
 import TextField from '@material-ui/core/TextField';
 import { Button, makeStyles, Theme, createStyles } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
 import { RequestStatus } from '../redux/slices/requests-slice';
 import { InlineIcon } from '@iconify/react';
 import Rotate from './rotate';
 import hourglassOutlined from '@iconify/icons-ant-design/hourglass-outlined';
-
 
 type Props = {
     command: string,
@@ -15,19 +14,55 @@ type Props = {
     isFetching: RequestStatus
 }
 
-const renderButton = (props: Props, spinnerClass: string) => {
+const renderButton = (props: Props, classes: Record<"spinner" | "generateButton", string>) => {
     const { isFetching, generateWord } = props
+    const { generateButton, spinner } = classes
+
     return isFetching === "OFF" ?
         <Button
+            className={generateButton}
             variant="contained"
             color="secondary"
             onClick={generateWord}>
             Generate</Button> :
-        <Rotate cyclic><InlineIcon className={spinnerClass} icon={hourglassOutlined} height="40" /></Rotate>
+        <div className={generateButton}>
+            <Rotate cyclic><InlineIcon className={spinner} icon={hourglassOutlined} height="38" /></Rotate>
+        </div>
 }
 
 const useStyle = makeStyles((theme: Theme) =>
     createStyles({
+        root: {
+            background: theme.palette.primary.main,
+            display: 'flex',
+            flexFlow: 'column nowrap',
+            justifyContent: 'space-around',
+            alignItems: 'stretch'
+        },
+        h1: {
+            margin: theme.spacing(3, 4, 1)
+        },
+        inputDiv: {
+            display: 'flex',
+            margin: theme.spacing(3, 0, 4),
+            '& > *': {
+                margin: theme.spacing(0, 1)
+            }
+        },
+        leftGap: {
+            flexGrow: 3
+        },
+        expessionInput: {
+            flexGrow: 6
+        },
+        generateButtonContainer: {
+            display: 'flex',
+            flexGrow: 2,
+            flexBasis: 2,
+        },
+        generateButton: {
+            margin: 'auto'
+        },
         spinner: {
             color: theme.palette.secondary.main,
             filter: 'drop-shadow( 0px 0px 2px rgba(0, 0, 0, .7))'
@@ -46,15 +81,15 @@ export default function Header(props: Props) {
         }
     }
 
-
     const classes = useStyle(props)
 
     return (
-        <div id="header">
-            <h1>Word generator</h1>
-            <div className="commandDiv">
+        <Card className={classes.root}>
+            <h1 className={classes.h1}>Word generator</h1>
+            <div className={classes.inputDiv}>
+                <div className={classes.leftGap} />
                 <TextField
-                    className="commandInput"
+                    className={classes.expessionInput}
                     label="Expression"
                     color="secondary"
                     size="small"
@@ -63,11 +98,10 @@ export default function Header(props: Props) {
                     onKeyPress={handleEnter}
                     variant="outlined" />
 
-                <div
-                    className="generate">
-                    {renderButton(props, classes.spinner)}
+                <div className={classes.generateButtonContainer}>
+                    {renderButton(props, classes)}
                 </div>
             </div>
-        </div>
+        </Card>
     )
 }
