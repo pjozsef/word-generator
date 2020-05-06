07c5aa76440a37928a0836014a7b614138057cc8
pjozsef <9980358+pjozsef@users.noreply.github.com>
2020-05-06 22:26:28 +0200

    feat(web-fe): remove empty lines from categories

--- word-generator-web/frontend/src/redux/async/generate-word.ts
+++ word-generator-web/frontend/src/redux/async/generate-word.ts
@@ -3,6 +3,13 @@ import { AppDispatch } from "../store"
 import { AppState } from "../app-state"
 import axios from 'axios'
 
+const removeEmptyLines = (mappings: object) => {
+  return Object.entries(mappings).map(([key, value]) => {
+    const filtered = (value as string[]).map((v)=>v.trim()).filter((v) => v !== '')
+    return { [key]: filtered }
+  }).reduce((curr, acc) => ({ ...acc, ...curr }), {})
+}
+
 export default createAsyncThunk<string[], void, {
   dispatch: AppDispatch
   state: AppState
@@ -11,13 +18,13 @@ export default createAsyncThunk<string[], void, {
   async (_, thunkAPI) => {
     const { getState } = thunkAPI
     const { command, categories: { all } } = getState()
-    const mappings = all.reduce((acc, curr) => { 
-      const e = {[curr.name]: curr.value.split('\n')} 
-      return {...acc, ...e}
+    const mappings = all.reduce((acc, curr) => {
+      const e = { [curr.name]: curr.value.split('\n') }
+      return { ...acc, ...e }
     }, {})
     const response = await axios.post('/api/generate', {
       expression: command,
-      mappings,
+      mappings: removeEmptyLines(mappings),
       times: 15
     })
     return response.data.results
