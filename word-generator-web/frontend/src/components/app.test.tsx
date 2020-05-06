60fa2e7919d947b190dcc6383514318845f42a3a
pjozsef <9980358+pjozsef@users.noreply.github.com>
2020-05-06 23:40:35 +0200

    test(web-fe): delete default dummy testcase

--- word-generator-web/frontend/src/components/app.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import {App} from './app';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const h1 = getByText(/Word generator/i);
-  expect(h1).toBeInTheDocument();
-});
